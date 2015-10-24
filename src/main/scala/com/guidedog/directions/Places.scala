273c84d889a26af31a76dc882bd21bc14ad6bde9
andrewsim <andrews@cakesolutions.net>
2015-10-24 17:40:53 +0100

    extract places

--- src/main/scala/com/guidedog/directions/Places.scala
+++ src/main/scala/com/guidedog/directions/Places.scala
@@ -10,9 +10,14 @@ object Places {
   val apiKey = "AIzaSyCiWUfRtqpWK2Y7xLqY-E9y7qZUB1ZuCew"
   val context = new GeoApiContext().setApiKey(apiKey)
 
-  def searchPlaces(description: String): Future[List[PlacesSearchResult]] = Future {
+  def searchPlaces(description: String, near: String): Future[List[PlacesSearchResult]] = Future {
     val req = PlacesApi.textSearchQuery(context, description)
+      .openNow(true)
+      .radius(1600) // roughly one mile radius
     req.await().results.toList
   }
+
+  def extractPlaces(xs: List[PlacesSearchResult]): List[String] =
+    xs.map(r => r.name)
 
 }
